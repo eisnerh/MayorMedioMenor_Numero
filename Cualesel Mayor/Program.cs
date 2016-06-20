c324db622e515a6c45970303a6bdde3bc112edb9
Eisner Lopez Acevedo <elopeza@ucatolica.ac.cr>
2016-06-20 11:27:27 -0600

    Programa Finalizado

--- Cualesel Mayor/Program.cs
+++ Cualesel Mayor/Program.cs
@@ -10,8 +10,35 @@ namespace Cualesel_Mayor
     {
         static void Main(string[] args)
         {
+            int num, a, b, c;
+            Console.WriteLine("Ingrese primer numero: ");
+            a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese segundo numero: ");
+            b = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese tercer numero: ");
+            c = Int32.Parse(Console.ReadLine());
 
-            Console.WriteLine("Ingrese un ");
+            if ((a > b) && (a > c))
+            {
+                Console.WriteLine("{0} es el mayor ", a);
+            }
+            else
+            {
+                if ((b > a) && (b > c))
+                {
+                    Console.WriteLine("{0} es el mayor ", b);
+                }
+
+                else
+                {
+                    if ((c > a) && (b > a))
+                    {
+                        Console.WriteLine("{0} es el mayor ", c);
+                    }
+                }
+            }
+            Console.ReadKey();
+
         }
     }
 }
